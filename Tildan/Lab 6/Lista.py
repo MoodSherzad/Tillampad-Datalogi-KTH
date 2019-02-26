70141a36a4c9c27429d2f9dee9cc6ed8e5a5eb07
hakansam <hakan.samanci@hotmail.com>
2019-02-26 11:06:29 +0100

    forts...

--- Tildan/Lab 6/Lista.py
+++ Tildan/Lab 6/Lista.py
@@ -1,14 +1,13 @@
 class Song:
     def __init__(self, track_id, artistnamn, sångtitel, låtlängd, år):
+        # artistid	artistnamn	sångtitel	låtlängd	år   (inläsnings strukturen)
         self.track_id = track_id
         self.artistnamn = artistnamn
         self.sångtitel = sångtitel
         self.låtlängd = låtlängd
         self.är = år
 
-    def __lt__(self, other):
-        return self.track_id < other.track_id
-#artistid	artistnamn	sångtitel	låtlängd	år
+
 
 def readfile(file_name):
     LIST_SONG = []
@@ -21,13 +20,16 @@ def readfile(file_name):
             DICT_SONG[row[3]] = song_object
     return LIST_SONG, DICT_SONG
         
-def nstorsta2(x, n):
+def nstorsta2(x, n): #x är listan och n är vilket tal vi vill ha
     list1 = []
     for i in range(n):
-        list1.append(x.pop(x.index(max(x.sångtid))))
-    print(list1[0])
+        list1.append(x.pop(x.index(max(x))))
+    print(list1[-1])
+    print(list1)
 
 
-filename = "unique_tracks.txt"
-lista, dictionary = readfile(filename)
-nstorsta2(lista,3)
+filename = "testlistan.txt" #behövs sen
+lista, dictionary = readfile(filename) #behövs sen
+
+testlistan = [2,34,5,67,88,7,65]
+nstorsta2(testlistan,3) #funktionen tar in testlistan och spottar ut 3 största elementet
