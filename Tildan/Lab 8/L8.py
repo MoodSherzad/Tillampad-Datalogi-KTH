46b5e16fee4a4df0b9229e9a88bc28dac4e79f6c
MoodMirani <47563174+MoodMirani@users.noreply.github.com>
2019-03-22 10:20:18 +0100

    whatever

--- Tildan/Lab 8/L8.py
+++ Tildan/Lab 8/L8.py
@@ -3,34 +3,66 @@ from linkedQFile import *
 class Syntaxfel(Exception):
     pass
 
-
+# Funktion molecule ska lagra alla symboler i en länkad lista
 def molecule(indata):
     q = LinkedQ()
     for i in indata:
             q.enqueue(i)
     return q
 
+# Funktion uppercase kontrollerar ifall bokstaven är en stor bokstav
 def uppercase(q):
     if q.peek().isupper():
         q.dequeue()
     else:
-        raise Syntaxfel("En stor bokstav saknas")
+        raise Syntaxfel("En stor bokstav saknas!")
 
+# Funktion lowercase kontrol
 def lowercase(q):
     if q.peek().islower():
         q.dequeue()
         number(q)
 
-def number(q):
+
+"""def number(q):
     if q.peek() is not None:
         if q.peek().isdigit():
             if q.peek() not in  ["1","0"]:
                 q.dequeue()
             else:
+                raise Syntaxfel("Siffran måste vara större än 1!")
+        else:
+            raise Syntaxfel("SyntaxFel")
+
+    """
+# kollar ifall siffer används rätt
+def number(q):
+    if q.peek() is not None:
+        if q.peek().isdigit():
+
+            if q.peek() is "0":
+                raise Syntaxfel("får ej börja med noll")
+            elif q.peek() is  "1":
+                q.dequeue()
+                if q.peek() is None:
+                    raise Syntaxfel("Siffran måste vara större än 1!")
+
+                else:
+                    while q.peek() is not None:
+                        if q.peek().isdigit():
+                            q.dequeue()
+                            pass
+                        else:
+                            raise Syntaxfel("SyntaxFel")
+            elif q.peek().isdigit():
+                q.dequeue()
+                number(q)
+            else:
                 raise Syntaxfel("Siffran måste vara större än 1")
         else:
             raise Syntaxfel("SyntaxFel")
 
+# kollar om molekylen har rätt syntax
 def syntax_control(indata):
     q = molecule(indata)
     try:
@@ -42,7 +74,7 @@ def syntax_control(indata):
     return "Formeln följer korrekt syntax!"
 
 def main():
-    indata = input("Skriv en molekyl: ").strip()
+    indata = input("Skriv en molekyl: ").strip() # kolla om den är tom
     resultat = syntax_control(indata)
     print(resultat)
     
@@ -51,3 +83,7 @@ if __name__ == '__main__':
 
 
         
+
+# Godkänd av Caroline Yu
+
+# Tjena Alexander och Julia
--- Tildan/Lab 8/Unitest.py
+++ Tildan/Lab 8/Unitest.py
@@ -5,15 +5,25 @@ from L8 import *
 
 class SyntaxTest(unittest.TestCase):
 
-    def testnumber(self):
-        """ Testar siffra """
-        self.assertEqual(number("3"), "Följer syntaxen!")
+    def test_syntax_correct(self):
 
-    def testfelnumber(self):
-        """ testtar felaktig siffta """
-        self.assertEqual(number("b"), "Följer ej syntaxen!")
-        
+        self.assertEqual(syntax_control("Ha3"), "Formeln följer korrekt syntax!")
+    
+    def test_no_uppercase(self):
 
+        self.assertEqual(syntax_control("ha3"), "En stor bokstav saknas!")
+
+    def test_digit(self):
+
+        self.assertEqual(syntax_control("Ha1"), "Siffran måste vara större än 1!")
+
+    def test(self):
+
+        self.assertEqual(syntax_control("HH3"), "SyntaxFel")
+
+    def test_3(self):
+
+        self.assertEqual(syntax_control("Ha1a"), "SyntaxFel")
 
 if __name__ == '__main__':
     unittest.main()
